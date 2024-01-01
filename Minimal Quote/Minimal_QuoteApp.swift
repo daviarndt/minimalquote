c19a86001363cc9c2c4d86ef2ef50e5509404681
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2024-01-01 01:46:43 -0300

    Initial Commit

---
+++ Minimal Quote/Minimal_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_QuoteApp.swift
+//  Minimal Quote
+//
+//  Created by Davi Arndt on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Minimal QuoteUITests/Minimal_QuoteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Minimal_QuoteUITestsLaunchTests.swift
+//  Minimal QuoteUITests
+//
+//  Created by Davi Arndt on 01/01/24.
+//
+
+import XCTest
+
+final class Minimal_QuoteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
